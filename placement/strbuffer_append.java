d7efe9f28239c1c06093150bd17320afad9d497a
sangezhil <144825015+sangezhil@users.noreply.github.com>
2024-10-28 14:43:10 +0530

    Add files via upload

---
+++ placement/strbuffer_append.java
@@ -0,0 +1,10 @@
+package placement;
+
+public class strbuffer_append {
+    public static void main(String[] args) {
+        StringBuffer a = new StringBuffer("hello");
+        a.append("java");
+        System.out.println(a);
+    }
+
+}
